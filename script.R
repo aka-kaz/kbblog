a65b0f65e4a7935d9f11c8b7dc3798fe2270d3a7
Kassie B <kbreest@umass.edu>
2023-05-04 15:44:06 -0400

    updated URL

--- script.R
+++ script.R
@@ -1,3 +1,7 @@
 install.packages("blogdown") # install the blogdown package
 library(blogdown) # load blogdown
 new_site(theme = "wowchemy/starter-academic") # create your website!
+
+install.packages("rstudioapi")
+library(rstudioapi) # to easily navigate to files
+rstudioapi::navigateToFile("config.yaml")
